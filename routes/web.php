e626732af69a262541ee21bac85dcaeaa98e4ba5
SarahAMarpaung <144139674+SarahAMarpaung@users.noreply.github.com>
2024-12-05 12:35:48 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PageController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [PageController::class, 'home'])->name('home');
+Route::get('/about-us', [PageController::class, 'about'])->name('about');
+Route::get('/coaches', [PageController::class, 'coaches'])->name('coaches');
+Route::get('/blog', [PageController::class, 'blog'])->name('blog');
+Route::get('/help', [PageController::class, 'help'])->name('help');
 });
